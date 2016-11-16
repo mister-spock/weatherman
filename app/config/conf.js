3e6d7161d4056fe196a3f5699a65b8621feb2863
Oleg Kozak <okozak@centraldesktop.com>
2016-11-16 19:20:08 +0200

    Initial commit

---
+++ app/config/conf.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const
+    API_HOST = 'api.openweathermap.org',
+    API_KEY  = "859e0f7c9f8bcccaf873f1a2b0085b04",
+    CITY_ID  = "706369",
+    ICON_URL = API_HOST + '/img/w/';
+
+module.exports = {
+    host     : API_HOST,
+    path     : `/data/2.5/weather?id=${CITY_ID}&units=metric&APPID=${API_KEY}`,
+    icon_url : ICON_URL
+};
